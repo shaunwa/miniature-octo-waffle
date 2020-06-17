2ed329d705aa4e4f8eba607978912209b043ffe1
Shaun W <shaun.p.wassell@gmail.com>
2020-06-16 20:25:47 -0400

    Updates

---
+++ precache-manifest.46cc61588b5aa1d2f0b772c8a8ba2f4a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9308de6c9d8f9e52aa7bcb89e8ed18f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "36c6a4912a3073fdecfd",
+    "url": "/static/css/main.8d831560.chunk.css"
+  },
+  {
+    "revision": "f8972a73e3c0c4d3333c",
+    "url": "/static/js/2.9243a75e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.9243a75e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36c6a4912a3073fdecfd",
+    "url": "/static/js/main.3e1598ac.chunk.js"
+  },
+  {
+    "revision": "5710aac846e8e9034b04",
+    "url": "/static/js/runtime-main.219f0b69.js"
+  }
+]);
